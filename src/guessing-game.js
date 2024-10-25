3b32d16c009c0a64b47d84eb1c7d19968594a112
Rixels <mikey.drozd@gmail.com>
2024-10-25 21:11:23 +0300

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = null;
+        this.max = null;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2);
     }
-
     lower() {
-
+        this.max = this.guess();
     }
-
     greater() {
-
+        this.min = this.guess();
     }
 }
-
 module.exports = GuessingGame;
